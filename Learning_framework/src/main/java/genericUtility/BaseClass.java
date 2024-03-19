dc2b84513d38bee0fc8d4d17746e393befe86582
ShonAntony <shon.antony99@gmail.com>
2024-03-19 15:03:52 +0530

    base class is edited

--- Learning_framework/src/main/java/genericUtility/BaseClass.java
+++ Learning_framework/src/main/java/genericUtility/BaseClass.java
@@ -1,6 +1,5 @@
 package genericUtility;
 
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -16,58 +15,62 @@ public class BaseClass {
 	DatabaseUtility eLib = new DatabaseUtility();
 	FileUtility fLib = new FileUtility();
 	WebDriverUtility wLib = new WebDriverUtility();
-	
+
 	public WebDriver driver;
 	public static WebDriver sdriver;
-	
 
-	// this method is to connect to the database before the suite execution 
+	// this method is to connect to the database before the suite execution
 	@BeforeSuite
 	public void dbConnect() {
 		System.out.println("the database is connected");
-		//dLib.connectToDB();
+		// dLib.connectToDB();
 	}
-	
-	
+
 	// this method is to launch the specific browser
 	@BeforeClass
 	public void configBC() {
-		/*
-		 * String browser = fLib.getValueFromPropertyFile("browser");
-		 * System.out.println("the browser is"+browser);
-		 * if(browser.equalsIgnoreCase("chrome")) { driver = new ChromeDriver(); } else
-		 * if(browser.equalsIgnoreCase("firefox")) { driver = new FirefoxDriver(); }
-		 * else { driver = new ChromeDriver(); } wLib.maximizeBrowserWindow(driver);
-		 * System.out.println("browser is launched");
-		 */
+
+		String browser = fLib.getValueFromPropertyFile("browser");
+		System.out.println("the browser is" + browser);
+		if (browser.equalsIgnoreCase("chrome")) {
+			driver = new ChromeDriver();
+		} else if (browser.equalsIgnoreCase("firefox")) {
+			driver = new FirefoxDriver();
+		} else {
+			driver = new ChromeDriver();
+		}
+		wLib.maximizeBrowserWindow(driver);
+		System.out.println("browser is launched");
+
 		System.out.println("browser opened");
 	}
-	
-	// this method is to login to the application 
+
+	// this method is to login to the application
 	@BeforeMethod
 	public void configBM() {
 		System.out.println("the user has logged in");
-		// here we need to get the username and the password from the property file and then use it 
+		// here we need to get the username and the password from the property file and
+		// then use it
 	}
-	
-	// this method is to logout from the application 
+
+	// this method is to logout from the application
 	@AfterMethod
 	public void configAM() {
 		System.out.println("the user has logged out");
 	}
-	
+
 	// this method is to close the browser
 	@AfterClass
 	public void configAC() {
 		/* driver.quit(); */
 		System.out.println("");
 	}
-	
-	//this method is to close the database connection 
+
+	// this method is to close the database connection
 	@AfterSuite
 	public void configAS() {
 		/* dLib.closeDB(); */
 		System.out.println("DB closed");
 	}
-	
+
 }
